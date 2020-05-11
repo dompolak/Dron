617acd7c33f6b9c8109db85fa9c85994cfc4529f
Dominik Polak <252923@student.pwr.edu.pl>
2020-05-11 10:10:43 +0200

    z

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -1,6 +1,12 @@
 #include "dron.hh"
 
+void wait4key() {
+  do {
+    cout << "\n Press a key to continue..." << endl;
+  } while(cin.get() != 'a');
+}
+
 int main()
 {
-    
+     drawNS::APIGnuPlot3D *wsk=new drawNS::APIGnuPlot3D(-5,5,-5,5,-5,5,1000);
 }
